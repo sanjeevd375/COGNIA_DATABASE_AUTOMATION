737f5f29dfecff1ed0585792401ba118728b82fe
Sanjeev Kumar Das <sanjeev.das@marlabs.com>
2021-04-30 23:53:36 +0530

    New table schema2

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+USE [Cognia_DEV]
+GO
+
+/****** Object:  Table [dbo].[Table_1]    Script Date: 30-04-2021 23:52:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Table_2](
+	[name] [nchar](10) NULL,
+	[address] [nvarchar](50) NULL,
+	[first_name] [nvarchar](50) NULL,
+	[last_name] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
